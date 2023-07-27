4404310d58bffc2c3b8d8d054eae00611165b5d3
Vagishmaurya <89206689+Vagishmaurya@users.noreply.github.com>
2023-07-27 13:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = 'https://social-media-client-gamma.vercel.app/';
+let origin = 'http://localhost:3000';
 console.log('here env', process.env.NODE_ENV);
-//if(process.env.NODE_ENV === 'production') {
-//    origin = process.env.CLIENT_ORIGIN;
-//}
+if(process.env.NODE_ENV === 'production') {
+    origin = process.env.CLIENT_ORIGIN;
+}
 app.use(
     cors({
         credentials: true,
